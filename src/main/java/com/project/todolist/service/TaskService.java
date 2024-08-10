be2c2428ad4200742a45cb9b798aa7234b06d80a
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-10 13:02:28 +0200

    aggiunto servizio Task

---
+++ src/main/java/com/project/todolist/service/TaskService.java
@@ -0,0 +1,29 @@
+package com.project.todolist.service;
+
+import com.project.todolist.model.Task;
+import com.project.todolist.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
